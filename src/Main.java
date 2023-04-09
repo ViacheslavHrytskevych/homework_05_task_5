30b408d1fdf7ccde1a8e989a9d6edf3b85c17234
Viacheslav Hrytskevych <hrytskevych.v@gmail.com>
2023-04-09 21:57:49 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("Greek");
+        list.add("was");
+        list.add("crossing");
+        list.add("a");
+        list.add("river");
+        list.add("and");
+        list.add("saw");
+        list.add("a");
+        list.add("Greek");
+        list.add("in");
+        list.add("the");
+        list.add("river");
+        list.add("with");
+        list.add("a");
+        list.add("crawfish");
+
+        List<Word> occurance = calcOccurance(list);
+
+        for (Word word : occurance) {
+            System.out.println(word.toString());
+        }
+    }
+
+    static List<Word> calcOccurance(List<String> list) {
+        list.sort(null);
+        int counter = 1;
+        List<Word> occurance = new ArrayList<>();
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            String current = list.get(i);
+            String next = list.get(i + 1);
+            if (current.equals(next)) {
+                counter++;
+            } else {
+                occurance.add(new Word(current, counter));
+                counter = 1;
+            }
+        }
+        String last = list.get(list.size() - 1);
+        occurance.add(new Word(last, counter));
+        return occurance;
+    }
 }
---
+++ src/Word.java
@@ -0,0 +1,31 @@
+public class Word {
+
+    private String name;
+    private int occurrence;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOccurrence() {
+        return occurrence;
+    }
+
+    public void setOccurrence(int occurrence) {
+        this.occurrence = occurrence;
+    }
+
+    public Word(String name, int occurrence) {
+        this.name = name;
+        this.occurrence = occurrence;
+    }
+
+    @Override
+    public String toString() {
+        return "{name: \"" + name + "\", occurrence: " + occurrence + "}";
+    }
+}
